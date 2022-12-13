c20ceb498e12761246090e63b70905cc10509d71
Sanara Felicio <sanara@cpqd.com.br>
2022-12-13 10:28:52 -0300

    aprendendo lambda e método sort e threads com lambda

--- src/projetoJava8/OrdenaStrings.java
+++ src/projetoJava8/OrdenaStrings.java
@@ -19,12 +19,12 @@ public class OrdenaStrings {
 		Collections.sort(palavras);
 
 		// cria um novo parâmetro para a ordenação da lista antigo
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		Collections.sort(palavras, comparador);
+//		Comparator<String> comparador = new ComparadorPorTamanho();
+//		Collections.sort(palavras, comparador);
 		System.out.println(palavras);
 
 		// novo método dentro da lista para ordenação
-		palavras.sort(comparador);
+//		palavras.sort(comparador);
 		System.out.println(palavras);
 
 		// iterando sobre umna lista com o for antigo
@@ -33,32 +33,64 @@ public class OrdenaStrings {
 		}
 
 		// nova forma de fazer uma iteração com java8
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+//		Consumer<String> consumidor = new ImprimeNaLinha();
+//		palavras.forEach(consumidor);
+
+		// declarando consumer no próprio método
+		palavras.forEach(new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s + " no corpo");
+			}
+		});
+
+		// método com lambda
+		palavras.forEach(s -> System.out.println(s + " no corpo"));
+
+		// passando um Compaartor de String
+		palavras.sort(new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				if (s1.length() < s2.length()) {
+					return -1;
+				}
+				if (s1.length() > s2.length()) {
+					return 1;
+				}
+				return 0;
+			}
+		});
+
+		// lambda para o sort
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		//consumidor com lambda
+		Consumer<String> impressor = s-> System.out.println(s);
+		palavras.forEach(impressor);
 
 	}
-}
 
-class ImprimeNaLinha implements Consumer<String> {
+	class ImprimeNaLinha implements Consumer<String> {
 
-	@Override
-	public void accept(String t) {
-		System.out.println(t);
-	}
-
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length()) {
-			return -1;
+		@Override
+		public void accept(String t) {
+			System.out.println(t);
 		}
-		if (s1.length() > s2.length()) {
-			return 1;
-		}
-		return 0;
+
 	}
 
+	class ComparadorPorTamanho implements Comparator<String> {
+
+		@Override
+		public int compare(String s1, String s2) {
+			if (s1.length() < s2.length()) {
+				return -1;
+			}
+			if (s1.length() > s2.length()) {
+				return 1;
+			}
+			return 0;
+		}
+
+	}
 }
